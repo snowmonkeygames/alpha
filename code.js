1aec9b3066f3e2416d03eb6a5f712d14543cad61
snowmonkey-jade <jeremy@snowmonkeygames.com>
2016-08-04 19:05:24 -0700

    Simple spinner demo

---
+++ code.js
@@ -0,0 +1,26 @@
+function init() {
+
+  var stage = new createjs.Stage("demoCanvas");
+  createjs.Ticker.addEventListener("tick", tick);
+
+  var spinner1 = new createjs.Shape();
+  spinner1.graphics.beginStroke('red').setStrokeStyle(5).arc(0,0, 20, 0, Math.PI/2);
+  spinner1.x = 100;
+  spinner1.y = 100;
+
+  var spinner2 = new createjs.Shape();
+  spinner2.graphics.beginStroke('blue').setStrokeStyle(5).arc(0,0, 20, 0, Math.PI/2);
+  spinner2.x = 100;
+  spinner2.y = 100;
+
+
+  stage.addChild(spinner1);
+  stage.addChild(spinner2);
+
+  function tick(event) {
+    spinner1.rotation += 1;
+    spinner2.rotation -= 1;
+    stage.update();
+  }
+
+}
