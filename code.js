c96e5e2e25705704199177597d1cc7e6b1a36602
snowmonkey-jade <jeremy@snowmonkeygames.com>
2016-08-04 19:10:43 -0700

    added transparency to spinners

--- code.js
+++ code.js
@@ -4,15 +4,14 @@ function init() {
   createjs.Ticker.addEventListener("tick", tick);
 
   var spinner1 = new createjs.Shape();
-  spinner1.graphics.beginStroke('red').setStrokeStyle(5).arc(0,0, 20, 0, Math.PI/2);
+  spinner1.graphics.beginStroke("rgba(0,0,255,0.5)").setStrokeStyle(5).arc(0,0, 20, 0, Math.PI/2);
   spinner1.x = 100;
   spinner1.y = 100;
 
   var spinner2 = new createjs.Shape();
-  spinner2.graphics.beginStroke('blue').setStrokeStyle(5).arc(0,0, 20, 0, Math.PI/2);
+  spinner2.graphics.beginStroke("rgba(255,0,0,0.5)").setStrokeStyle(5).arc(0,0, 20, 0, Math.PI/2);
   spinner2.x = 100;
   spinner2.y = 100;
-
 
   stage.addChild(spinner1);
   stage.addChild(spinner2);
