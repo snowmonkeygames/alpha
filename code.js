ac64e81084ca94900b03e48ee7a64b5564bc64d2
snowmonkey-jade <jeremy@snowmonkeygames.com>
2016-08-07 17:13:03 -0700

    cleaned up

--- code.js
+++ code.js
@@ -7,11 +7,12 @@ function init() {
 
   var pongBall = new createjs.Bitmap("pongball.png");
   pongBall.shadow = new createjs.Shadow("#0022ff", 2, 2, 10);
-  var ballDirection = "up";
   pongBall.x = (pongCanvas.width / 2) - 25;
   pongBall.y = (pongCanvas.height / 2) - 25;
+  var ballDirection = "up";
 
   var bg = new createjs.Bitmap("bg.png").set({x:0,y:0});
+
   var topBarBlue = new createjs.Bitmap("top-blue.png").set({x:0,y:0});
   var bottomBarBlue = new createjs.Bitmap("bottom-blue.png").set({x:0,y:pongCanvas.height - 100});
   var topBarOrange = new createjs.Bitmap("top-orange.png").set({x:0,y:0});
@@ -44,6 +45,7 @@ function init() {
     }
     if (bottomHit == true && timer == false){
       timer = true;
+      stage.removeChild(bottomBarBlue);
       stage.addChild(bottomBarOrange);
     }
     if (topHit == true && timer == false){
